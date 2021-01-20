1a265817e2675c0c482ad02df7cfd0bb61b8cf3e
Dmitry Komarov <dimakomarov2012@gmail.com>
2021-01-20 22:39:42 +0300

    complete day1 tasks

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -5,9 +5,9 @@ import java.security.spec.RSAOtherPrimeInfo;
 public class Task1 {
     public static void main(String[] args) {
         int value = 0;
-        while(value < 10){
+        while (value < 10) {
             System.out.print("JAVA ");
-            value = value+1;
+            value = value + 1;
         }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -4,9 +4,10 @@ public class Task6 {
     public static void main(String[] args) {
         int k = 3;
 
-for(int p = 1; p<=9; p++)
-    {int j = p*k;
-    System.out.println(p +"*"+k+"="+j); }
+        for (int p = 1; p <= 9; p++) {
+            int j = p * k;
+            System.out.println(p + "*" + k + "=" + j);
+        }
     }
 }
 
