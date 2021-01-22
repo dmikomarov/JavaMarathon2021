5f6358a18d9083355388eb96403100267b7e81d4
Dmitry Komarov <dimakomarov2012@gmail.com>
2021-01-22 13:00:48 +0300

    Add day2 solution

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число а");
+        int a = scanner.nextInt();{
+        System.out.println("Введите число b");}
+        int b = scanner.nextInt();
+        if (a >= b || a < 0) {
+        System.out.println("Некорректный ввод");}
+        for (int c = 0; c < b; c++)
+            if (c % 5 == 0)
+                if (c % 10 != 0) {
+                    System.out.println(c + " ");
+                }
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,27 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число а");
+        int a = scanner.nextInt();
+        System.out.println("Введите число b");
+        int b = scanner.nextInt();
+        if (a >= b || a < 0) {
+            System.out.println("Некорректный ввод");
+        }
+        int c = 0;
+
+        while (c < b) {
+            if (c % 5 == 0)
+                if (c % 10 != 0) {
+                    System.out.println(c + " ");
+                }
+            c = c + 1;
+        }
+
 
     }
 }
