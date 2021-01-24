88e0ac73c639b53872505f77e92a258e328ec2f3
Dmitry Komarov <dimakomarov2012@gmail.com>
2021-01-24 20:59:45 +0300

    Add day3 solution

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,24 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int c = 0;
+
+        while (c < 5) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            c++;
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(a / b);
+
+        }
 
     }
 }
+
