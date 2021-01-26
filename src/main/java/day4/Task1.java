ab100a9885911a49b429809537c10165db770d7d
Dmitry Komarov <dimakomarov2012@gmail.com>
2021-01-27 01:19:06 +0300

    Add Day4 solution

--- src/main/java/day4/Task1.java
+++ src/main/java/day4/Task1.java
@@ -1,7 +1,39 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        Random random = new Random();
+        int[] y = new int[x];
+        int bolshevosmi = 0;
+        int ravnoedinice = 0;
+        int counter2 = 0;
+        int counter3 = 0;
+        int counter4 = 0;
+        for (int i = 0; i < y.length; i++) {
+            y[i] = random.nextInt(11);
+            if (y[i] > 8)
+                bolshevosmi++;
+            if (y[i] == 1)
+                ravnoedinice++;
+            if (y[i] % 2 == 0)
+                counter2++;
+            if (y[i] % 2 != 0)
+                counter3++;
+            counter4 += y[i];
+        }
+        System.out.println(Arrays.toString(y));
+        System.out.println("Больше восьми" + " " + bolshevosmi);
+        System.out.println("Равно единице" + " " + ravnoedinice);
+        System.out.println("Четные числа" + " " + counter2);
+        System.out.println("Нечетные числа" + " " + counter3);
+        System.out.println("Сумма" + " " + counter4);
+
 
     }
 }
--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -1,7 +1,32 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        int[] array = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++)
+            array[i] = random.nextInt(10000);
+        int max = 0;
+        int min = 10000;
+        int counter = 0;
+        int sum = 0;
+        for (int element : array) {
+            if (element > max)
+                max = element;
+            if (element < min)
+                min = element;
+            if (element % 10 == 0)
+                counter++;
+            if (element % 10 == 0)
+                sum += element;
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(counter + " ");
+        System.out.println(sum);
     }
 }
